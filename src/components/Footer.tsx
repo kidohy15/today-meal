8649ccac3047b068bc24c20e7c7872f435893846
kidohy15 <kidohy307@gmail.com>
2024-03-01 04:44:21 +0900

    style: footer ui 수정

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,29 +1,29 @@
 export default function Footer() {
   return (
-    <footer>
-      <div className="flex flex-col bg-slate-800/90 w-full h-[300px] text-white p-20 text-center gap-2">
-        <ul className="flex justify-center gap-2">
-          <li>
-            <p>개인정보처리방침</p>
-          </li>
-          <li>
-            <p>홈페이지 이용약관</p>
-          </li>
-        </ul>
-
+    <footer className="w-full bg-slate-800/90 flex justify-center">
+      <div className="justify-start w-full md:max-w-6xl h-[300px] px-16 py-12 text-gray-400 text-lg  text-start gap-2 leading-5">
+        <div className="mb-4">
+          <span className="inline-block text-orange-300 font-bold text-3xl mr-3 py-3">
+            YummyRecipe
+          </span>
+          <p className="leading-5 text-gray-400">
+            레시피를 추천해주고 공유하는 사이트입니다. <br />
+            해당 사이트는 비영리목적의 개발자 개인 프로젝트입니다.
+          </p>
+        </div>
         <div>
-          <span>개발자 김동현 </span>
+          <p>개발자: 김동현 </p>
         </div>
 
         <div>
-          {/* <p>연락처: 010-1111-1111</p> */}
-          <p>이메일: test@test.com</p>
+          <p>Email: test@test.com</p>
         </div>
-
-        <p>
-          &copy; YummyRecipe. All Rights
-          Reserved.
-        </p>
+        <div className="whitespace-pre-wrap ">
+          <p className="">
+            &copy; YummyRecipe. 음식 이미지의 경우 구글 이미지를 사용하고
+            있습니다.
+          </p>
+        </div>
       </div>
     </footer>
   );
