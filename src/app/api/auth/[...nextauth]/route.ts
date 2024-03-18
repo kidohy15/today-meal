11fc7b30f274005d0093c3354ebb0a992d3d64e5
kidohy15 <kidohy307@gmail.com>
2024-03-19 02:56:56 +0900

    build: auth 빌드 에러 수정 중

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,7 @@
 import NextAuth from "next-auth";
 import AuthOptions from "@/app/lib/auth";
 
+// @ts-ignore
 export const authOptions = AuthOptions
 const handler = NextAuth(authOptions);
 export { handler as GET, handler as POST };
