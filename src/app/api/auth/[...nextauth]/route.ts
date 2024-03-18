912d604022259e0df741060a96d0a8d3c7d75415
kidohy15 <kidohy307@gmail.com>
2024-03-19 02:40:42 +0900

    build: auth 빌드 에러 수정중

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,9 +1,4 @@
-import NextAuth, { NextAuthOptions, AuthOptions  } from "next-auth";
-import { PrismaClient } from "@prisma/client";
-import { PrismaAdapter } from "@next-auth/prisma-adapter";
-import GoogleProvider from "next-auth/providers/google";
-import CredentialsProvider from "next-auth/providers/credentials";
-import bcrypt from "bcrypt";
+import NextAuth from "next-auth";
 import authOptions from "@/app/lib/auth";
 
 const handler = NextAuth(authOptions);
