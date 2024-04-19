05522595381ad24fc56ef516fb057f9212190e99
kidohy15 <kidohy307@gmail.com>
2024-04-20 02:21:20 +0900

    config: 미들웨어 옵션 수정

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,5 +1,13 @@
 export { default } from "next-auth/middleware";
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
 
 export const config = {
   matcher: ["/recipe/new", "/recipe/:id/edit", "/users/likes", "/users/mypage"],
 };
+
+export function middleware(request: NextRequest) {
+  if (request.nextUrl.pathname.startsWith('/api/auth/signin')) {
+    return NextResponse.rewrite(new URL('/', request.url))
+  }
+}
